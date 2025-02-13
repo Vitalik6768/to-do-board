0ccfed30cdc38ea181f2eb6b7efde0126084e053
Vitalik6768 <98703332+Vitalik6768@users.noreply.github.com>
2025-02-13 11:51:25 +0200

    Update page.tsx

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -4,7 +4,18 @@ import KanbanBoard from "./components/KanbanBoard";
 export default function Home() {
   return (
     <div>
+      <div className="text-white text-right text-2xl font-bold px-4 py-2">
+        <h3>
+          {new Date().toLocaleDateString('en-US', {
+            month: 'long',
+            day: 'numeric',
+            year: 'numeric'
+          })}
+        </h3>
+      </div>
+      <div>
       <KanbanBoard />
+      </div>
     </div>
   );
 }
